334aad4fbab79e0ca67c63c8da73e1ceb340de37
Akash <goravanakollakash@gmail.com>
2024-09-02 16:32:48 +0530

    whatsup button

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import Header from './components/Navbar/Header/Header';
 import Home from './components/HomePage/Home';
 import Footer from './components/Navbar/Footer/Footer';
+import WhatsAppButton from './components/WhatsupButton/WhatsupButton';
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <Home/>
-      <Footer/>
-    </div>
+    <>
+      <div>
+        <Header />
+        <Home />
+        <Footer />
+      </div>
+      <WhatsAppButton/>
+    </>
   );
 }
 
